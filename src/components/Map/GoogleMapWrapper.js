affcee55d66dc8785fb4e3f46c61b20a98475c8c
Damian Wojakowski <wojakowski.damian@gmail.com>
2020-11-09 16:24:35 +0100

    code refactoring, GoogleMapWrapper component

--- src/components/Map/GoogleMapWrapper.js
+++ src/components/Map/GoogleMapWrapper.js
@@ -11,39 +11,14 @@ export default class GoogleMapWrapper extends Component{
 
     constructor(props) {
         super(props);
-
-        this.onMapClick = this.onMapClick.bind(this);
-
-        // const {isLoaded, loadError} = useLoadScript({
-        //     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-        // });
-
-        // this.mapRef = React.useRef();
-        // this.onMapLoad = React.useCallback((map) => {
-        //     this.mapRef.current = map;
-        // }, []);
-
-        // this.panTo = React.useCallback((lat, lng) => {
-        //     this.mapRef.current.panTo({lat, lng});
-        // }, []);
-
         this.state = {
             markers: []
         };
 
-        //
-        // const [selected, setSelected] = React.useState(null);
-        // const [markers, setMarkers] = React.useState([]);
-        // const [CENTER, setCenter] = React.useState({
-        //     lat: 51.107883,
-        //     lng: 17.038538
-        // });
-        //
+        this.onMapClick = this.onMapClick.bind(this);
     }
 
     onMapClick(event) {
-        // this.panTo(event.latLng.lat(), event.latLng.lng());
-
         this.setState((state, props) => {
             return {
                 markers: [...state.markers, {
@@ -53,18 +28,9 @@ export default class GoogleMapWrapper extends Component{
                 }]
             }
         });
-
     }
 
     render() {
-        // if (loadError) {
-        //     return "Error loading maps";
-        // }
-        //
-        // if (!isLoaded) {
-        //     return "Loading Maps...";
-        // }
-
         return (
             <div>
                 <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
